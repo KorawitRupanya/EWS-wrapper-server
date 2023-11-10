42d6dc0267c59e8ca39ca67f77f5e8c34ee521a0
Korawit Rupanya <32285708+KorawitRupanya@users.noreply.github.com>
2023-11-10 21:43:44 +0100

    Remove commented code

--- app/resources.py
+++ app/resources.py
@@ -72,27 +72,6 @@ class Adaptation(Resource):
         except Exception as e:
             return jsonify({'error': str(e)}), 500            
 
-# @ns.route("/monitor_schema")
-# class MonitorSchema(Resource):
-#     def get(self):
-#         try:
-#             # Make a request to the external API
-#             external_api_url = 'http://host.docker.internal:2011/meta/get_perception'
-#             response = requests.get(external_api_url)
-#             # Check if the request to the external API was successful
-#             if response.status_code == 200:
-#                 data = response.json()
-#                 # Generate JSON schema from the received JSON response
-#                 builder = SchemaBuilder()
-#                 builder.add_object(data)
-#                 schema = builder.to_schema()
-#                 # Return the generated schema
-#                 return jsonify(schema)
-#             else:
-#                 return jsonify({'error': 'Failed to fetch data from the external API'}), 500
-#         except Exception as e:
-#             return jsonify({'error': str(e)}), 500
-
 @ns.route("/monitor_schema")
 class MonitorSchema(Resource):
     def get(self):
